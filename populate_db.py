45c3e106f829e75954cf6dae95f02165709959fc
vagrant <vagrant@vagrant.vm>
2019-08-03 06:57:19 +0000

    Add shebangs to database and populate_db.py

--- populate_db.py
+++ populate_db.py
@@ -1,13 +1,23 @@
+#!/usr/bin/env python3
+import csv
+
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 from database_setup import Sighting, SightingType
+
+DEMO_DATA_FILE = 'sightings_data.csv'
+
 
 # Hook up db engine to a Session class
 engine = create_engine('sqlite:///wildsight.db')
 Session = sessionmaker(bind=engine)
 # Instantiate a session to access the db
 session = Session()
+
+# Add sighting type table
+#['Flower', 'Tree', 'Plant', 'Air Animal']
+
 
 # TODO convert csv file to database adding thing
 
@@ -16,3 +26,4 @@ session = Session()
 # - 
 # - session.add_all([List of Sighting/Sighting Type objects])
 # - session.commit()
+
