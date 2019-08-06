c4fcb518497ad0be6145e10d5941e76289f91cdf
vagrant <vagrant@vagrant.vm>
2019-08-06 21:46:12 +0000

    Finish populate_db.py for adding demo data to the db.

--- populate_db.py
+++ populate_db.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
-import csv
+import pandas as pd
 
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 from database_setup import Sighting, SightingType
 
+# Read in some demo data
 DEMO_DATA_FILE = 'sightings_data.csv'
-
+demo_data = pd.read_csv(DEMO_DATA_FILE)
 
 # Hook up db engine to a Session class
 engine = create_engine('sqlite:///wildsight.db')
@@ -15,15 +16,26 @@ Session = sessionmaker(bind=engine)
 # Instantiate a session to access the db
 session = Session()
 
-# Add sighting type table
-#['Flower', 'Tree', 'Plant', 'Air Animal']
+# Create sighting type objects based on sighting_type column
+# and place into a dict.
+sighting_type_dict = {sighting_type:SightingType(type = sighting_type)
+                     for sighting_type in demo_data.sighting_type.unique()}
+
+# Add and commit all sighting types
+for i in sighting_type_dict:
+    session.add(sighting_type_dict[i])
+    session.commit()
+
+# Add and commit all sightings
+for i in demo_data.index:
+    new_sighting = Sighting(title = demo_data.loc[i,:]['title'], 
+                            description= demo_data.loc[i,:]['description'],
+                            location = demo_data.loc[i,:]['location'],
+                            sighting_type = sighting_type_dict[demo_data.loc[i,:]['sighting_type']])
+    session.add(new_sighting)
+    session.commit()
 
 
-# TODO convert csv file to database adding thing
+session.close()
 
-# Soemthing like
-# - read the csv
-# - 
-# - session.add_all([List of Sighting/Sighting Type objects])
-# - session.commit()
 
