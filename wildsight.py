5fe510a8d0b1ddca7d2ffcf2a710cd0fe58d8fcc
vagrant <vagrant@vagrant.vm>
2019-08-12 06:35:07 +0000

    Add edit sighting feature.

--- wildsight.py
+++ wildsight.py
@@ -22,10 +22,9 @@ def home():
     return render_template('index.html', sightings=sightings)
 
 # Placeholder pages to create, edit, and delete pages
-@app.route('/sightings/new/', methods=['GET', 'POST'])
+@app.route('/sightings/new/', methods = ['GET', 'POST'])
 def create_sighting():
     if request.method == 'POST':
-        # TODO add sighting type query
         sighting_type = session.query(SightingType).filter_by(type = request.form['sighting_type']).one()
         new_sighting = Sighting(title = request.form['title'],
                                 description = request.form['description'],
@@ -37,11 +36,33 @@ def create_sighting():
         
     else:     
         sighting_types = session.query(SightingType)
-        return render_template('new_sighting.html', sighting_types=sighting_types)
+        return render_template('new_sighting.html', sighting_types = sighting_types)
 
-@app.route('/sightings/<type>/<int:id>/edit/')
+@app.route('/sightings/<type>/<int:id>/edit/', methods = ['GET', 'POST'])
 def edit_sighting(type, id):
-    return 'edit sighting'
+    if request.method == 'POST':
+        sighting = session.query(Sighting).filter_by(id=id).one()
+        sighting_type = session.query(SightingType).filter_by(type = request.form['sighting_type']).one()
+        # Update required fields only if not empty
+        if request.form['title']:
+            sighting.title = request.form['title']
+        if request.form['location']:
+            sighting.location = request.form['location']
+        if request.form['sighting_type']:
+            sighting.sighting_type = sighting_type
+        # Description is not required
+        sighting.description = request.form['description']
+
+        return redirect(url_for('home'))
+
+    else:
+        sighting = session.query(Sighting).filter_by(id = id).one()
+        current_sighting_type = sighting.sighting_type
+        other_sighting_types = session.query(SightingType).filter(SightingType.id != current_sighting_type.id)
+        return render_template('edit_sighting.html', 
+                                sighting=sighting, 
+                                current_sighting_type=current_sighting_type,
+                                other_sighting_types=other_sighting_types)
 
 @app.route('/sightings/<type>/<int:id>/delete/')
 def delete_sighting(type, id):
@@ -60,8 +81,8 @@ def list_types():
 # Displays sightings of a particular type
 @app.route('/types/<type>/')
 def list_sightings_in_type(type):
-    sighting_type = session.query(SightingType).filter_by(type = type).one()
-    sightings = session.query(Sighting).filter_by(sighting_type = sighting_type)
+    sighting_type = session.query(SightingType).filter_by(type=type).one()
+    sightings = session.query(Sighting).filter_by(sighting_type=sighting_type)
     output = ''
     for i in sightings:
         output += i.title
