c7350346c0909652eb30ea8572f5024a7864f1a9
vagrant <vagrant@vagrant.vm>
2019-08-17 21:54:24 +0000

    Change sighting form description to TextAreaField.

--- sighting_form.py
+++ sighting_form.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField
+from wtforms import StringField, SubmitField, SelectField, TextAreaField
 from wtforms.validators import DataRequired, Length
 
 class SightingForm(FlaskForm):
@@ -9,7 +9,7 @@ class SightingForm(FlaskForm):
     MS_LEN = "Maximum %(max)d characters."
     # Form fields
     title = StringField('Title', validators=[DataRequired(message=MS_REQ), Length(max=250, message=MS_LEN)])
-    description = StringField('Description', validators=[Length(max=2000, message=MS_LEN)])
+    description = TextAreaField('Description', validators=[Length(max=2000, message=MS_LEN)])
     location = StringField('Location', validators=[DataRequired(message=MS_REQ), Length(max=200, message=MS_LEN)])
     sighting_type = SelectField('Sighting Type', coerce=int, validators=[DataRequired(message=MS_REQ)])
     submit = SubmitField('Submit!')
