7868a996f9e3175b659efb781766cd5bf6917772
vagrant <vagrant@vagrant.vm>
2019-08-19 05:10:54 +0000

    Add serialize functions to database.

--- database_setup.py
+++ database_setup.py
@@ -14,7 +14,18 @@ class SightingType(Base):
 
     id = Column(Integer, primary_key = True)
     type = Column(String(250), nullable = False)
+    # Location of image file
     image = Column(String(250), nullable = False)
+
+    # For data access via JSON
+    @property
+    def serialize(self):
+        """Return object data for easy conversion to JSON"""
+        return {
+            'id': self.id,
+            'type': self.type,
+            'image': self.image
+        }
 
 class Sighting(Base):
     __tablename__ = 'sighting'
@@ -26,7 +37,19 @@ class Sighting(Base):
     sighting_type_id = Column(Integer, ForeignKey('sighting_type.id'))
     sighting_type = relationship(SightingType)
 
-## TODO investigate serialization function.
+    # For data access via JSON
+    @property
+    def serialize(self):
+        """Return object data for easy conversion to JSON"""
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'location': self.location,
+            'sighting_type_id': self.sighting_type_id,
+            # Str representing sighting type
+            'sighting_type': self.sighting_type.type
+        }
 
 # SQLite dialect
 engine = create_engine('sqlite:///wildsight.db')
