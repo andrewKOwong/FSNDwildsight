58d27766bc43afdf4bf88e400b015015cb55eae6
vagrant <vagrant@vagrant.vm>
2019-08-22 06:41:27 +0000

    Add demo_data and database code to handle users.

--- database_setup.py
+++ database_setup.py
@@ -9,6 +9,14 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
 class SightingType(Base):
     __tablename__ = 'sighting_type'
 
@@ -36,6 +44,8 @@ class Sighting(Base):
     location = Column(String(200), nullable = False)
     sighting_type_id = Column(Integer, ForeignKey('sighting_type.id'))
     sighting_type = relationship(SightingType)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     # For data access via JSON
     @property
@@ -50,6 +60,8 @@ class Sighting(Base):
             # Str representing sighting type
             'sighting_type': self.sighting_type.type
         }
+
+ 
 
 # SQLite dialect
 engine = create_engine('sqlite:///wildsight.db')
--- populate_db.py
+++ populate_db.py
@@ -4,13 +4,15 @@ import pandas as pd
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-from database_setup import Sighting, SightingType, Base
+from database_setup import Sighting, SightingType, Base, User
 
 # Read in some demo data
 DEMO_DATA_SIGHTINGS_FILE = 'demo_data/sightings_data.csv'
 DEMO_DATA_TYPE_FILE = 'demo_data/sighting_type_data.csv'
+DEMO_DATA_USERS_FILE = 'demo_data/user_data.csv'
 demo_data = pd.read_csv(DEMO_DATA_SIGHTINGS_FILE)
 demo_types = pd.read_csv(DEMO_DATA_TYPE_FILE)
+demo_users = pd.read_csv(DEMO_DATA_USERS_FILE)
 
 # Hook up db engine to a Session class
 engine = create_engine('sqlite:///wildsight.db')
@@ -18,6 +20,14 @@ Session = sessionmaker(bind=engine)
 Base.metadata.bind = engine
 # Instantiate a session to access the db
 session = Session()
+
+# Create user database
+for i in demo_users.index:
+    data = demo_users.loc[i,:]
+    new_user = User(name=data['user_name'],
+                    email=data['email'])
+    session.add(new_user)
+    session.commit()
 
 
 # Create sighting type objects
@@ -34,10 +44,12 @@ for i in demo_types.index:
 for i in demo_data.index:
     data = demo_data.loc[i,:]
     sighting_type = session.query(SightingType).filter_by(type=data['sighting_type']).one()
+    user = session.query(User).filter_by(id=int(data['user_id'])).one()
     new_sighting = Sighting(title=data['title'], 
                             description=data['description'],
                             location=data['location'],
-                            sighting_type=sighting_type)
+                            sighting_type=sighting_type,
+                            user=user)
     session.add(new_sighting)
     session.commit()
 
