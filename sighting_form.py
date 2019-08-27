e19518e73e71a0bc9deb4609dc48b7802e412ae0
vagrant <vagrant@vagrant.vm>
2019-08-27 21:21:38 +0000

    Cleanup sighting_form.py

--- sighting_form.py
+++ sighting_form.py
@@ -3,13 +3,22 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, TextAreaField
 from wtforms.validators import DataRequired, Length
 
+
 class SightingForm(FlaskForm):
     # error messages, MS is short for message
     MS_REQ = "This field required!"
     MS_LEN = "Maximum %(max)d characters."
     # Form fields
-    title = StringField('Title', validators=[DataRequired(message=MS_REQ), Length(max=250, message=MS_LEN)])
-    description = TextAreaField('Description', validators=[Length(max=2000, message=MS_LEN)])
-    location = StringField('Location', validators=[DataRequired(message=MS_REQ), Length(max=200, message=MS_LEN)])
-    sighting_type_id = SelectField('Sighting Type', coerce=int, validators=[DataRequired(message=MS_REQ)])
+    title = StringField('Title',
+                        validators=[DataRequired(message=MS_REQ),
+                                    Length(max=250, message=MS_LEN)])
+    description = TextAreaField('Description',
+                                validators=[Length(max=2000,
+                                            message=MS_LEN)])
+    location = StringField('Location',
+                           validators=[DataRequired(message=MS_REQ),
+                                       Length(max=200, message=MS_LEN)])
+    sighting_type_id = SelectField('Sighting Type',
+                                   coerce=int,
+                                   validators=[DataRequired(message=MS_REQ)])
     submit = SubmitField('Submit!')
