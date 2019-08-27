13206d8d70026d908fed98fe1ffcc1cf74a57410
vagrant <vagrant@vagrant.vm>
2019-08-27 21:17:17 +0000

    Clean up populate_db.py

--- populate_db.py
+++ populate_db.py
@@ -7,6 +7,7 @@ from sqlalchemy import create_engine
 from database_setup import Sighting, SightingType, Base, User
 
 # Read in some demo data
+# Pandas is convenient for handling csvs
 DEMO_DATA_SIGHTINGS_FILE = 'demo_data/sightings_data.csv'
 DEMO_DATA_TYPE_FILE = 'demo_data/sighting_type_data.csv'
 DEMO_DATA_USERS_FILE = 'demo_data/user_data.csv'
@@ -23,7 +24,7 @@ session = Session()
 
 # Create user database
 for i in demo_users.index:
-    data = demo_users.loc[i,:]
+    data = demo_users.loc[i, :]
     new_user = User(name=data['user_name'],
                     email=data['email'])
     session.add(new_user)
@@ -32,7 +33,7 @@ for i in demo_users.index:
 
 # Create sighting type objects
 for i in demo_types.index:
-    data = demo_types.loc[i,:]
+    data = demo_types.loc[i, :]
     new_type = SightingType(type=data['type'],
                             image=data['image'])
     session.add(new_type)
@@ -42,10 +43,11 @@ for i in demo_types.index:
 # Add and commit all sightings
 # Use query to get the sighting type for each sighting
 for i in demo_data.index:
-    data = demo_data.loc[i,:]
-    sighting_type = session.query(SightingType).filter_by(type=data['sighting_type']).one()
+    data = demo_data.loc[i, :]
+    sighting_type = session.query(SightingType).filter_by(
+                                type=data['sighting_type']).one()
     user = session.query(User).filter_by(id=int(data['user_id'])).one()
-    new_sighting = Sighting(title=data['title'], 
+    new_sighting = Sighting(title=data['title'],
                             description=data['description'],
                             location=data['location'],
                             sighting_type=sighting_type,
@@ -55,5 +57,3 @@ for i in demo_data.index:
 
 
 session.close()
-
-
